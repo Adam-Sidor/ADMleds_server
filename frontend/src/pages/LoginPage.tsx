4bee1187b677cbecb49254608eafb8bb14afe9c8
Adam Sidor <sidoadsi1@gmail.com>
2025-10-20 22:40:21 +0200

    Frontend connected to backend via LoginPage

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,10 +1,34 @@
 import React from "react";
+import axios from 'axios';
 
 export function LoginPage() {
+  const backendIP = 'localhost';
+
+  const login = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    const username = formData.get('username')?.toString() || '';
+    const password = formData.get('password')?.toString() || '';
+    try{
+      const res = await axios.get('http://' + backendIP + ':8080/api/user/login', {params:{username,password}})
+      console.log(res.data);
+    }
+    catch(error){
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>Logowanie</h1>
-      {/* formularz logowania */}
+      <form onSubmit={login}>
+        <input type="text" id="username" name="username" placeholder="Username"></input><br></br>
+        <input type="password" id="password" name="password" placeholder="Password"></input><br></br>
+        <input type="submit"></input>
+        <input type="reset"></input>
+      </form>
     </div>
   );
 }
