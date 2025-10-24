ee98f05d9964b259161de7daa9a4ad3724c43304
Adam Sidor <sidoadsi1@gmail.com>
2025-10-24 18:59:17 +0200

    NewDeviceForm RegEx verification

--- frontend/src/forms/newDeviceForm.tsx
+++ frontend/src/forms/newDeviceForm.tsx
@@ -18,6 +18,10 @@ export function NewDeviceForm({ backendIP, userDevices, token }: NewDeviceFormPr
     const [newDeviceStatus, setNewDeviceStatus] = useState("");
     const [deviceTypes, setDeviceTypes] = useState<DeviceType[]>([]);
     const [isDevice, setIsDevice] = useState(false);
+
+    const [ipError, setIpError] = useState(false);
+    const [typeError, setTypeError] = useState(false);
+
     const getDeviceTypes = async () => {
         try {
             const res = await axios.post('http://' + backendIP + ':8080/api/device/getalltypes', {});
@@ -33,36 +37,47 @@ export function NewDeviceForm({ backendIP, userDevices, token }: NewDeviceFormPr
         const form = event.currentTarget;
         const formData = new FormData(form);
 
-        const ip = formData.get("IP");
-        const type = formData.get("type");
+        const ip = formData.get("IP")?.toString() || "";
+        const type = formData.get("type")?.toString() || "";
 
+        const ipv4Regex = /^(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})\.(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})\.(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})\.(25[0-5]|2[0-4][0-9]|1?[0-9]{1,2})$/;
 
-
-        userDevices.map((uDevice) => (
-            uDevice.deviceIp === ip ? setIsDevice(true) : ""
-        ));
-        if (!isDevice) {
-            try {
-                const res = await axios.post('http://' + backendIP + ':8080/api/device/new', { ip, type });
-                console.log(res.data);
-                setNewDeviceStatus(res.data);
-            } catch (error) {
-                console.log(error);
+        if (type === "") {
+            setTypeError(true);
+        } else {
+            setTypeError(false);
+        }
+        if (ipv4Regex.test(ip)) {
+            setIpError(false);
+            if (type !== "") {
+                userDevices.map((uDevice) => (
+                    uDevice.deviceIp === ip ? setIsDevice(true) : ""
+                ));
+                if (!isDevice) {
+                    try {
+                        const res = await axios.post('http://' + backendIP + ':8080/api/device/new', { ip, type });
+                        console.log(res.data);
+                        setNewDeviceStatus(res.data);
+                    } catch (error) {
+                        console.log(error);
+                    }
+                } else {
+                    setIsDevice(false);
+                    console.log("urządzenie istnieje");
+                    try {
+                        await axios.post(`http://${backendIP}:8080/api/usersettings/updatestate`, {
+                            token,
+                            deviceIp: ip,
+                            newState: "added"
+                        });
+                    } catch (error) {
+                        console.error(error);
+                    }
+                }
             }
         } else {
-            setIsDevice(false);
-            console.log("urządzenie istnieje");
-            try {
-                await axios.post(`http://${backendIP}:8080/api/usersettings/updatestate`, {
-                    token,
-                    deviceIp: ip,
-                    newState: "added"
-                });
-            } catch (error) {
-                console.error(error);
-            }
+            setIpError(true);
         }
-
     }
 
     useEffect(() => {
@@ -74,6 +89,7 @@ export function NewDeviceForm({ backendIP, userDevices, token }: NewDeviceFormPr
             <h3>Dodaj nowe urządzenie:</h3>
             <form onSubmit={createNewDevice}>
                 <input name="IP" placeholder="IP" /><br />
+                {ipError && <span>Adres IP musi mieć format xx.xx.xx.xx !<br /></span>}
                 <select name="type" defaultValue="">
                     <option value="" disabled>Wybierz typ urządzenia</option>
                     {deviceTypes.map((device) => (
@@ -82,6 +98,7 @@ export function NewDeviceForm({ backendIP, userDevices, token }: NewDeviceFormPr
                         </option>
                     ))}
                 </select><br />
+                {typeError && <span>Wybierz typ urządzenia!<br/></span>}
                 {newDeviceStatus}
                 <input type="submit" value="Stwórz" />
             </form>
