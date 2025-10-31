24ac385649fbd7098c27b88a9a02b2418ccc8fe6
Adam Sidor <sidoadsi1@gmail.com>
2025-10-31 16:29:09 +0100

    CORS update

--- backend/src/main/java/com/lightserver/backend/config/WebConfig.java
+++ backend/src/main/java/com/lightserver/backend/config/WebConfig.java
@@ -14,8 +14,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(System.getenv().getOrDefault("FRONTEND_URL", "http://localhost:5173"))
-                        .allowedMethods("*");
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
